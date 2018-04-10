a18c6e431de2f8ed3b7e44aa6d2d0349ba6b1e10
jwang1013 <37950852+jwang1013@users.noreply.github.com>
2018-04-09 17:57:59 -0700

    midway

---
+++ timestamp.js
@@ -0,0 +1,23 @@
+// basic required imports for NodeJS
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+// Creat an instance of express for our app and instantiate bodyParser and cors
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/dateValues', function(req, res, next){
+//GET call to return JSON that formats natural and unix date
+    console.log('URL works');
+});
+
+
+
+
+
+
+app.listen(3000, function (){
+    console.log('it is working');
+});
